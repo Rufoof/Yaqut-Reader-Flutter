fb36f1ad26e63eb3d3b9b8192747d7de5e0eca00
amersmadi <amer@rufoof.com>
2024-09-30 08:53:35 +0300

    Fix YaqutReaderBook model

--- test/yaqut_reader_flutter_test.dart
+++ test/yaqut_reader_flutter_test.dart
@@ -1,19 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:yaqut_reader_flutter/yaqut_reader_flutter.dart';
-import 'package:yaqut_reader_flutter/yaqut_reader_flutter_platform_interface.dart';
-import 'package:yaqut_reader_flutter/yaqut_reader_flutter_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:yaqut_reader_plugin/yaqut_reader_flutter.dart';
+import 'package:yaqut_reader_plugin/yaqut_reader_flutter_method_channel.dart';
+import 'package:yaqut_reader_plugin/yaqut_reader_flutter_platform_interface.dart';
 
 class MockYaqutReaderFlutterPlatform
     with MockPlatformInterfaceMixin
     implements YaqutReaderFlutterPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-  final YaqutReaderFlutterPlatform initialPlatform = YaqutReaderFlutterPlatform.instance;
+  final YaqutReaderFlutterPlatform initialPlatform =
+      YaqutReaderFlutterPlatform.instance;
 
   test('$MethodChannelYaqutReaderFlutter is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelYaqutReaderFlutter>());
@@ -21,7 +21,8 @@ void main() {
 
   test('getPlatformVersion', () async {
     YaqutReaderFlutter yaqutReaderFlutterPlugin = YaqutReaderFlutter();
-    MockYaqutReaderFlutterPlatform fakePlatform = MockYaqutReaderFlutterPlatform();
+    MockYaqutReaderFlutterPlatform fakePlatform =
+        MockYaqutReaderFlutterPlatform();
     YaqutReaderFlutterPlatform.instance = fakePlatform;
 
     expect(await yaqutReaderFlutterPlugin.getPlatformVersion(), '42');
