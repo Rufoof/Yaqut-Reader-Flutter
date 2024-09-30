7e3986a0474fe0935438b95eeb2e6cd42311ac33
rula@rufoof.com <rula@yaqut.me>
2024-09-30 03:03:24 -0400

    fix YaqutReaderPlugin

--- android/src/main/java/co/reader/yaqut_reader_flutter/YaqutReaderPlugin.java
+++ android/src/main/java/co/reader/yaqut_reader_flutter/YaqutReaderPlugin.java
@@ -1,123 +1,210 @@
-package android.src.main.java.co.reader.yaqut_reader_flutter;
+package co.reader.yaqut_reader_flutter;
+
+import static java.security.AccessController.getContext;
 
 import androidx.annotation.NonNull;
 
+import co.yaqut.reader.api.ReaderListener;
+import co.yaqut.reader.api.ReadingSession;
+import co.yaqut.reader.api.StatsSessionListener;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Parcel;
 import android.view.View;
+
 import androidx.annotation.NonNull;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
-import com.example.yaqut_reader.ReaderBuilder;
-import com.example.yaqut_reader.SaveBookManager;
-import com.example.yaqut_reader.BookStorage;
-import com.example.yaqut_reader.NotesAndMarks;
-import com.example.yaqut_reader.ReaderStyle;
-import com.example.yaqut_reader.ReaderDelegate;
-import com.example.yaqut_reader.StatsSessionDelegate;
+import co.yaqut.reader.api.ReaderBuilder;
+import co.yaqut.reader.api.SaveBookManager;
+import co.yaqut.reader.api.BookStorage;
+import co.yaqut.reader.api.NotesAndMarks;
+import co.yaqut.reader.api.ReaderStyle;
 
 import java.util.List;
 import java.util.Map;
 
 
-/** YaqutReaderFlutterPlugin */
+/**
+ * YaqutReaderFlutterPlugin
+ */
 public class YaqutReaderPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    private Context context;
 
     private ReaderBuilder readerBuilder;
     private Integer bookId;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-      channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "yaqut_reader_plugin");
-      channel.setMethodCallHandler(this);
-      setAppearance();
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "yaqut_reader_plugin");
+        channel.setMethodCallHandler(this);
+        setAppearance();
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-      channel.setMethodCallHandler(null);
+        channel.setMethodCallHandler(null);
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-      switch (call.method) {
-        case "getPlatformVersion":
-          result.success("Android " + android.os.Build.VERSION.RELEASE);
-          break;
-        case "saveBook":
-          String header = (String) arguments.get("header");
-          String path = (String) arguments.get("path");
-          String token = (String) arguments.get("access_token");
-          saveBook(path, header,token);
-          break;
-        case "startReader":
-          Map<String, Object> arguments = call.arguments();
-          Map<String, Object> book = (Map<String, Object>) arguments.get("book");
-          Map<String, Object> style = (Map<String, Object>) arguments.get("style");
-          startReader( book, style);
-          break;
-        case "checkIfLocal":
-          Map<String, Object> checkArgs = call.arguments();
-          int bookId = (Integer) checkArgs.get("book_id");
-          int bookFileId = (Integer) checkArgs.get("book_file_id");
-          BookStorage bookStorage = new BookStorage();
-          boolean isLocal = bookStorage.isBookLocal(bookId);
+        switch (call.method) {
+            case "getPlatformVersion":
+                result.success("Android " + android.os.Build.VERSION.RELEASE);
+                break;
+            case "saveBook":
+                String header = (String) arguments.get("header");
+                String path = (String) arguments.get("path");
+                String token = (String) arguments.get("access_token");
+                saveBook(path, header, token);
+                break;
+            case "startReader":
+                Map<String, Object> arguments = call.arguments();
+                Map<String, Object> book = (Map<String, Object>) arguments.get("book");
+                Map<String, Object> style = (Map<String, Object>) arguments.get("style");
+                startReader(book, style);
+                break;
+            case "checkIfLocal":
+                Map<String, Object> checkArgs = call.arguments();
+                int bookId = (Integer) checkArgs.get("book_id");
+                int bookFileId = (Integer) checkArgs.get("book_file_id");
+                boolean isLocal = BookStorage.isBookLocal(context,bookId);
 
-          result.success(isLocal);
-          break;
-        default:
-          result.notImplemented();
-      }
+                result.success(isLocal);
+                break;
+            default:
+                result.notImplemented();
+        }
     }
 
-    private void startReader( Map<String, Object> bookData, Map<String, Object> styleData) {
-      bookId = (Integer) bookData.get("bookId");
-      String title = (String) bookData.get("title");
-      int bookFileId = (Integer) ((Map<String, Object>) bookData.get("currentFile")).get("bookFileId");
-      double previewPercentage = (Double) bookData.getOrDefault("samplePreviewPercentage", 0.15);
-      int position = (Integer) bookData.get("position");
+    private void startReader(Map<String, Object> bookData, Map<String, Object> styleData) {
+        bookId = (Integer) bookData.get("bookId");
+        String title = (String) bookData.get("title");
+        int bookFileId = (Integer) ((Map<String, Object>) bookData.get("currentFile")).get("bookFileId");
+        double previewPercentage = (Double) bookData.getOrDefault("samplePreviewPercentage", 0.15);
+        int position = (Integer) bookData.get("position");
 
-      // Handle Reader Style
-      int readerColor = (Integer) styleData.getOrDefault("readerColor", 0);
-      int textSize = (Integer) styleData.getOrDefault("textSize", 22);
-      boolean isJustified = (Boolean) styleData.getOrDefault("isJustified", true);
-      int lineSpacing = (Integer) styleData.getOrDefault("lineSpacing", 1);
-      int font = (Integer) styleData.getOrDefault("font", 0);
+        // Handle Reader Style
+        int readerColor = (Integer) styleData.getOrDefault("readerColor", 0);
+        int textSize = (Integer) styleData.getOrDefault("textSize", 22);
+        boolean isJustified = (Boolean) styleData.getOrDefault("isJustified", true);
+        int lineSpacing = (Integer) styleData.getOrDefault("lineSpacing", 1);
+        int font = (Integer) styleData.getOrDefault("font", 0);
 
-      ReaderStyle readerStyle = new ReaderStyle(textSize, readerColor, isJustified, lineSpacing, font);
+        ReaderStyle readerStyle = new ReaderStyle(textSize, readerColor, isJustified ? 1 : 0, lineSpacing, font);
 
-      readerBuilder = new ReaderBuilder(getContext(), mBook.getBookId());
-      readerBuilder.setReaderStyle(readerStyle)
-              .setTitle(title)
-              .setCover(userBook.getCoverPath())
-              .setPosition(position)
-              .setPercentageView(previewPercentage)
-              .setReaderListener(this)
-              .setReadingStatsListener(this);
-      readerBuilder.setFileId(bookFileId);
-      readerBuilder.setNotesAndMarks(readerNotesAndMarks);
-      readerBuilder.setSaveState(getSaveState()).setDownloadEnabled(getDownloadState());
-      readerBuilder.build();
+        readerBuilder = new ReaderBuilder(context, bookId);
+        readerBuilder.setReaderStyle(readerStyle)
+                .setTitle(title)
+                .setCover("")
+                .setPosition(position)
+                .setPercentageView((float) previewPercentage)
+                .setReaderListener(readerListener)
+                .setReadingStatsListener(statListener);
+        readerBuilder.setFileId(bookFileId);
+        readerBuilder.setNotesAndMarks(null);
+        readerBuilder.setSaveState(ReaderBuilder.SAVE_STATE_NOT_SAVED).setDownloadEnabled(false);
+        readerBuilder.build();
     }
 
     private void setAppearance() {
-      // Handle UI customization here for Android equivalent
-      // Colors and appearances can be customized via views and themes
+        // Handle UI customization here for Android equivalent
+        // Colors and appearances can be customized via views and themes
     }
-  private void saveBook(String bodyPath, String header, String accessToken) {
-    SaveBookManager.save(getApplicationContext(), downloadRequest.getBookId(), bodyPath, header, accessToken);
-  }
-  }
+
+    private void saveBook(String bodyPath, String header, String accessToken) {
+        SaveBookManager.save(context, bookId, bodyPath, header, accessToken);
+    }
+    ReaderListener readerListener = new ReaderListener() {
+        @Override
+        public void onStyleChanged(ReaderStyle readerStyle) {
+
+        }
+
+        @Override
+        public void onPositionChanged(int i) {
+
+        }
+
+        @Override
+        public void onSyncNotesAndMarks(List<NotesAndMarks> list) {
+
+        }
+
+        @Override
+        public void onUpdateLastOpened(long l) {
+
+        }
+
+        @Override
+        public void onShareBook() {
+
+        }
+
+        @Override
+        public void onBookDetailsCLicked() {
+
+        }
+
+        @Override
+        public void onSaveBookClicked(int i) {
+
+        }
+
+        @Override
+        public void onDownloadBook() {
+
+        }
+
+        @Override
+        public void onReaderClosed(int i) {
+
+        }
+
+        @Override
+        public void onSampleEnded() {
+
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(@NonNull Parcel dest, int flags) {
+
+        }
+    };
+    StatsSessionListener statListener  = new StatsSessionListener() {
+        @Override
+        public void onReadingSessionEnd(ReadingSession readingSession) {
+
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(@NonNull Parcel dest, int flags) {
+
+        }
+    };
+}
--- yaqut_reader_flutter/android/app/src/main/java/co/reader/yaqut_reader_flutter/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package co.reader.yaqut_reader_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
