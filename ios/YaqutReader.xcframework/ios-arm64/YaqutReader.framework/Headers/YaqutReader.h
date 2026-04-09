084e0cdfa15129a6a8fe5ae428deed0d7d07d1bf
amersmadi <amer@rufoof.com>
2026-04-09 07:30:50 +0300

    reader view + audio player + mini audio player refactoring

---
+++ ios/YaqutReader.xcframework/ios-arm64/YaqutReader.framework/Headers/YaqutReader.h
@@ -0,0 +1,19 @@
+//
+//  YaqutReader.h
+//  YaqutReader
+//
+//  Created by Amer Smadi on 09/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for YaqutReader.
+FOUNDATION_EXPORT double YaqutReaderVersionNumber;
+
+//! Project version string for YaqutReader.
+FOUNDATION_EXPORT const unsigned char YaqutReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YaqutReader/PublicHeader.h>
+
+
